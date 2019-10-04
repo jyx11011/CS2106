3488b3665a04ad81830800b65fe3404b636b9d79
Jiang Yuxin <jyx11011@gmail.com>
2019-10-04 18:07:58 +0800

    lab3ex4

--- lab3/ex2/ex2.c
+++ lab3/ex2/ex2.c
@@ -1,8 +1,8 @@
 /*************************************
  * Lab 3 Exercise 2
- * Name: 
- * Student No:
- * Lab Group:
+ * Name: Jiang Yuxin
+ * Student No: A0177868A
+ * Lab Group: 11
  *************************************/
 #include <pthread.h>
 #include "rw_lock.h"
@@ -12,25 +12,36 @@ void initialise(rw_lock* lock)
   pthread_mutex_init(&(lock->mutex), NULL);
   lock->reader_count = 0;
   lock->writer_count = 0;
+  pthread_mutex_init(&(lock->empty),NULL);
+  pthread_mutex_init(&(lock->writer_mutex),NULL);
 }
 
 void writer_acquire(rw_lock* lock)
 {
-  pthread_mutex_lock(&(lock->mutex));
+  pthread_mutex_lock(&(lock->writer_mutex));
+  pthread_mutex_lock(&(lock->empty));
+  //pthread_mutex_lock(&(lock->mutex));
   lock->writer_count++;
-  pthread_mutex_unlock(&(lock->mutex));
+  //pthread_mutex_unlock(&(lock->mutex));
 }
 
 void writer_release(rw_lock* lock)
 {
-  pthread_mutex_lock(&(lock->mutex));
+  //pthread_mutex_lock(&(lock->mutex));
   lock->writer_count--;
-  pthread_mutex_unlock(&(lock->mutex));
+  pthread_mutex_unlock(&(lock->writer_mutex));
+  pthread_mutex_unlock(&(lock->empty));
+  //pthread_mutex_unlock(&(lock->mutex));
 }
 
 void reader_acquire(rw_lock* lock)
 {
+  pthread_mutex_lock(&(lock->writer_mutex));
+  pthread_mutex_unlock(&(lock->writer_mutex));
   pthread_mutex_lock(&(lock->mutex));
+  if(lock->reader_count==0) {
+    pthread_mutex_lock(&(lock->empty));
+  }
   lock->reader_count++;
   pthread_mutex_unlock(&(lock->mutex));
 }
@@ -39,10 +50,15 @@ void reader_release(rw_lock* lock)
 {
   pthread_mutex_lock(&(lock->mutex));
   lock->reader_count--;
+  if(lock->reader_count==0) {
+    pthread_mutex_unlock(&(lock->empty));
+  }
   pthread_mutex_unlock(&(lock->mutex));
 }
 
 void cleanup(rw_lock* lock)
 {
   pthread_mutex_destroy(&(lock->mutex));
+  pthread_mutex_destroy(&(lock->empty));
+  pthread_mutex_destroy(&(lock->writer_mutex));
 }
